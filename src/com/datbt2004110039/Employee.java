07d0291ccb151332e5af2ce81f279fcbda6cbc3a
DatBuiTien18102002 <datbui18102002@gmail.com>
2022-01-13 14:05:53 +0700

    Lần đẩy đầu tiên

---
+++ src/com/datbt2004110039/Employee.java
@@ -0,0 +1,25 @@
+package com.datbt2004110039;
+
+import java.util.Scanner;
+
+public class Employee {
+    String EmName;
+    int salary;
+    Scanner sc = new Scanner(System.in);
+
+    void getEmployeeName(){
+        System.out.print("Nhap ten nhan vien: ");
+        EmName = sc.nextLine();
+
+    }
+
+    void getSalary(){
+        System.out.print("Nhap luong cua nhan vien: ");
+        salary = sc.nextInt();
+    }
+
+    void showInformation(){
+        System.out.println("===Ten khach hang: "+ EmName);
+        System.out.println("===Dia chi: "+ salary);
+    }
+}
---
+++ src/com/datbt2004110039/customerTestDrive.java
@@ -0,0 +1,11 @@
+package com.datbt2004110039;
+
+public class customerTestDrive {
+    public static void main(String[] args) {
+        customer khachHang = new customer();
+
+        khachHang.getCusName();
+        khachHang.getAddress();
+        khachHang.showInformation();
+    }
+}
