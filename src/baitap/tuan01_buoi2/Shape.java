5a3b6698e9f3bd8b80a1ebbd2461e3b527a881b2
DatBuiTien18102002 <datbui18102002@gmail.com>
2022-01-16 21:07:51 +0700

    Up bài tập tuần 1 buổi 2

---
+++ src/baitap/tuan01_buoi2/Shape.java
@@ -0,0 +1,24 @@
+package baitap.tuan01_buoi2;
+
+public class Shape {
+    
+    String shape;
+    int size;
+    String color;
+    String audio;
+
+    Shape(String sh, int s, String c, String a){
+        shape = sh;
+        size = s;
+        color = c;
+        audio = a;
+    }
+
+    void rotateClockwise(){
+
+    }
+
+    void playAudio(){
+
+    }
+}
