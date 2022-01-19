2db5a90f87149985fcdf5aa2953a7e20d272cb8d
DatBuiTien18102002 <datbui18102002@gmail.com>
2022-01-19 22:06:26 +0700

    Bài tập tuần 2 buổi 1

---
+++ src/baitap/tuan02_buoi1/DogTestDrive.java
@@ -0,0 +1,14 @@
+package baitap.tuan02_buoi1;
+
+public class DogTestDrive {
+    
+    public static void main(String[] args) {
+
+        Dog newDog = new Dog();
+
+        newDog.bark();
+        newDog.setBigger();
+        newDog.run();
+        
+    }
+}
