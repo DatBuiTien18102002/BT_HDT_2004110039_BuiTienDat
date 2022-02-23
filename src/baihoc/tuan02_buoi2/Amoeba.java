afd9d87a637420fde69131a4332e7a31a976a11c
DatBuiTien18102002 <datbui18102002@gmail.com>
2022-02-23 21:32:50 +0700

    Cập nhật Encapsulation cho bài Shape

--- src/baihoc/tuan02_buoi2/Amoeba.java
+++ src/baihoc/tuan02_buoi2/Amoeba.java
@@ -1,21 +1,21 @@
 package baihoc.tuan02_buoi2;
 
 public class Amoeba {
-    String soundFile;
-    double xPoint;
-    double yPoint;
+    private String soundFile;
+    private double xPoint;
+    private double yPoint;
 
     Amoeba(String soundFile, double xPoint, double yPoint){
         this.soundFile = soundFile;
     }
 
-    void rotate(){
+    public void rotate(){
         System.out.println("Xoay hinh 360 do theo chieu kim dong ho tai X: " + xPoint + " y: " + yPoint);
         this.playSound();
     }
 
 
-    void playSound(){
+    public void playSound(){
         System.out.println("Phat am thanh tu file " + this.soundFile);
     }
 }
--- src/baihoc/tuan02_buoi2/Circle.java
+++ src/baihoc/tuan02_buoi2/Circle.java
@@ -2,19 +2,19 @@ package baihoc.tuan02_buoi2;
 
 public class Circle {
     
-    String soundFile;
+    private String soundFile;
 
     Circle(String soundFile){
         this.soundFile = soundFile;
     }
 
-    void rotate(){
+    public void rotate(){
         System.out.println("Xoay hinh 360 do theo chieu kim dong ho");
         this.playSound();
     }
 
 
-    void playSound(){
+    public void playSound(){
         System.out.println("Phat am thanh tu file " + this.soundFile);
     }
 }
