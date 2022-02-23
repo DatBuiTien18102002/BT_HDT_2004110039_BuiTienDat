a04a19b1e7a708377df9be8003547ddc29d39e56
DatBuiTien18102002 <datbui18102002@gmail.com>
2022-02-23 21:11:40 +0700

    Bài học tuần 3 buổi 1

---
+++ src/baihoc/tuan03_buoi1/IntSquareTestDrive.java
@@ -0,0 +1,11 @@
+package baihoc.tuan03_buoi1;
+
+public class IntSquareTestDrive {
+    public static void main(String[] args) {
+        IntSquare intSquare = new IntSquare();
+        double result  = intSquare.getSquare(2);
+        System.out.println(result);
+
+
+    }
+}
